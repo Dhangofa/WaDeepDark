1c0165971af57a27e777c828d73cfdedd5fadd9e
Dhangofa <117024745+Dhangofa@users.noreply.github.com>
2026-05-22 17:55:38 +0530

    Update build.gradle.kts [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.dhangofa.wadeepdark"
-        minSdk = 29
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -20,9 +20,11 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 dependencies {
